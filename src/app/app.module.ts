26486c1e48b1152d615186e033eb347c126dc77a
Aziz <aichaziz159@gmail.com>
2020-12-02 10:43:31 +0100

    detailmateriel ajouté

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { AccueilComponent } from './composants/accueil/accueil.component';
 import { ListmaterielComponent } from './composants/informatique/listmateriel/listmateriel.component';
 import { MaterielComponent } from './composants/informatique/materiel/materiel.component';
+import { DetailmaterielComponent } from './composants/informatique/detailmateriel/detailmateriel.component';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,7 @@ import { MaterielComponent } from './composants/informatique/materiel/materiel.c
     AccueilComponent,
     ListmaterielComponent,
     MaterielComponent,
-    
+    DetailmaterielComponent,  
   ],
   imports: [
     BrowserModule,
